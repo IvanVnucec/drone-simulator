ce38d9553184bec4008d2e128794d6767076b90e
IvanVnucec <vnucec.ivan@gmail.com>
2022-05-27 05:17:31 -0700

    add Eigen library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "main.hpp"
+#include "Eigen/Core"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using Eigen::MatrixXd;
+ 
+int main()
+{
+    MatrixXd m(2,2);
+
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    
+    std::cout << m << std::endl;
 }
